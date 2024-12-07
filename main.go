d05661f14dcfd3e9198e810dfd3aa6c642a90072
Rubens.F <xansxxl@gmail.com>
2024-12-07 14:48:31 -0300

    feat: add global messages chat

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/coder/websocket"
+)
+
+var (
+	clients map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
 )
 
 func webHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,23 +20,35 @@ func webHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	clients[con] = true
+
 	for {
 		_, data, err := con.Read(r.Context())
 		if err != nil {
 			log.Fatal(err)
+			delete(clients, con)
 			break
 		}
 
 		log.Println(string(data))
 
-		con.Write(r.Context(), websocket.MessageText, []byte("pong"))
+		message := string(data)
+
+		for client := range clients {
+			client.Write(r.Context(), websocket.MessageText, []byte(message))
+		}
+
 	}
 
 }
 
 func main() {
 
-	http.HandleFunc("/", webHandler)
+	http.HandleFunc("/ws", webHandler)
+
+	http.HandleFunc("/clients-count", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(strconv.Itoa(len(clients))))
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
